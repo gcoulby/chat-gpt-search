b4f91ddfd6720c54204d07f6a73956b99ee4658a
Graham Coulby <gcoulby@googlemail.com>
2024-03-11 21:52:42 +0000

    action update

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3000
-  }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/chat-gpt-search/" : "/",
 })
